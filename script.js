982495be79cacdffed588e4e06525c16e32e26e3
Vladimir Saenko <56477695+VladimirSaenko@users.noreply.github.com>
2022-01-22 16:27:01 +0200

    add site

---
+++ script.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let section = document.querySelector('section');
+let text = document.querySelector('.text');
+let innerText = document.querySelector('.innerText');
+let loremtext = document.getElementById("loremtext");
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    section.style.clipPath = `circle(${value}px at center center)`;
+    text.style.left = 100 - value / 5 + `%`;
+    innerText.style.left = 100 - value / 5 + `%`;
+})
+
+loremtext.scrollIntoView();
